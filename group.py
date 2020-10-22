838d6c4ba0ceb768828c826b04b70fa7f0fec01f
kyrlazari <48288612+kyrlazari@users.noreply.github.com>
2020-10-22 10:59:05 +0100

    Create a dictionary as a friend group

--- group.py
+++ group.py
@@ -1,5 +1,22 @@
-"""An example of how to represent a group of acquaintances in Python."""
+"An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
 
-my_group =
+my_group = {"Kyriazis":{"age":25,
+                        "job":"Msc Student",
+                        "friends":["Mathew","Michael"]
+                        },
+            "Mathew":{"age":24,
+                    "job":"Phd Student",
+                    "friends":["Kyriazis","Michael"]
+                    },
+            "Michael":{"age":25,
+                    "job":"Phd Student",
+                    "friends":["Kyriazis","Mathew"]
+                    },
+            "Emre":{"age":22,
+                    "job":"Mres",
+                    "friends":["Kyriazis","Michael","Mathew"]
+                    }        
+            } 
+print(my_group["Emre"]["friends"][2])                               
