041e9e14f61d7f2ca7d0f46cbf974e3ed851e758
HU <uacpkhu@ucl.ac.uk>
2020-10-22 18:17:32 +0800

    add data

--- group.py
+++ group.py
@@ -2,4 +2,9 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [
+    {'id': 1, 'name': 'Jill', 'job': 'Biologist', 'age': 26, 'relations': [{'to': 2, 'type': 'Friend'}, {'to': 3, 'type': 'Partner'}]},
+    {'id': 2, 'name': 'Zalika', 'job': 'Artist', 'age': 28, 'relations': [{'to': 1, 'type': 'Friend'}]},
+    {'id': 3, 'name': 'John', 'job': 'Writer', 'age': 27, 'relations': [{'to': 1, 'type': 'Partner'}]},
+    {'id': 4, 'name': 'Nash', 'job': 'Chef', 'age': 34, 'relations': [{'to': 3, 'type': 'Cousin'}, {'to': 2, 'type': 'Landlord'}]},
+]
