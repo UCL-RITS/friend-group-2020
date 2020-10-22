3edd2ea8f011bff83d48dfbef853228a9151ecd7
Aure <zcemaen@ucl.ac.uk>
2020-10-22 11:08:24 +0100

    first commit

--- group.py
+++ group.py
@@ -2,4 +2,25 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [{'name':Jill, 'age':26, }
+
+class Person():
+    def __init__(self, name, age, job, connection=[] ):
+        self.name = name
+        self.age = age
+        self.job =job
+        self.connection = connection
+
+    def add_connection(name, relationship)
+        connection= self.connection.append({'name':name, 'relationship':relationship})
+        return connection
+
+    
+
+
+jill=Person('Jill', 26, 'biologist')
+zalika = Person('Zalika', 28, 'artist')
+
+
+Person('John', 27, 'writer')
+Person('Nash', 34, 'chef')
