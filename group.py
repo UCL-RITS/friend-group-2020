4281ab2a2408a2436dd3a9a7cb8e191e90e7f2b8
Tim <timothy.weaving.20@ucl.ac.uk>
2020-10-22 13:39:25 +0100

    Added code to calculate various age metrics

--- group.py
+++ group.py
@@ -1,5 +1,3 @@
-"""An example of how to represent a group of acquaintances in Python."""
-
 my_group = {
     "Jill": {
         "age": 26,
@@ -32,3 +30,14 @@ my_group = {
         }
     }
 }
+
+ages = [my_group[key]['age'] for key in my_group.keys()]
+ages_rel = [my_group[key]['age'] for key in my_group.keys() if my_group[key]['relations']] #includes age if relations not empty (however in this case all entries have at least one relation)
+ages_frnd = [my_group[key]['age'] for key in my_group.keys() if 'friend' in my_group[key]['relations'].values()]
+
+
+print("Maximum overall age is %i" % max(ages))
+print("Average overall age is %f" % (sum(ages)/len(ages)))
+print("Maximum age of those with at least one relative is %i" % max(ages_rel))
+print("Maximum age of those with at least one friend is %i" % max(ages_frnd))
+
