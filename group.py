10d9d1b8334ae0450813018be043b8b7f5dd9b54
Aure <zcemaen@ucl.ac.uk>
2020-10-22 11:38:04 +0100

    Added connection function. Still to add: 2-way relationship

--- group.py
+++ group.py
@@ -2,25 +2,28 @@
 
 # Your code to go here...
 
-my_group = [{'name':Jill, 'age':26, }
 
-class Person():
-    def __init__(self, name, age, job, connection=[] ):
+class Person:
+    def __init__(self, name, age, job, connection={} ):
         self.name = name
         self.age = age
         self.job =job
         self.connection = connection
 
-    def add_connection(name, relationship)
-        connection= self.connection.append({'name':name, 'relationship':relationship})
-        return connection
-
-    
+    def add_connection(self, friend, relationship):
+        self.connection.update({friend.name:relationship})
+        if relationship=='friend' or relationship=='partner':
+            friend.connection.update({self.name:relationship})
 
 
 jill=Person('Jill', 26, 'biologist')
 zalika = Person('Zalika', 28, 'artist')
+john=Person('John', 27, 'writer')
+nash=Person('Nash', 34, 'chef')
 
+jill.add_connection(zalika, 'friend')
+jill.add_connection(john, 'partner')
+print(zalika.connection)
+print(jill.connection)
+#zalika.add_connection(jill, 'friend')
 
-Person('John', 27, 'writer')
-Person('Nash', 34, 'chef')
