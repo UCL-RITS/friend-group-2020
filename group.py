3835c3a862bda35d4e18a8bd8b9993ebfc406991
Snigdha <Snigdha@snigdhasmacbook.home>
2020-10-22 11:35:32 +0100

    create friend network in group.py file

--- group.py
+++ group.py
@@ -1,5 +1,10 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+friendNetworkDict = {
+	"Jill" : { "Age" : 26, "Occupation" : "Biologist", "Connections" : { "Zalika" : "friend", "John" : "partner" }}
+	"Zalika" : { "Age" : 28, "Occupation" : "Artist", "Connections" : { "Jill" : "friend"}}
+	"John" : { "Age" : 27, "Occupation" : "Writer", "Connections" : { "Jill" : "partner" }}
+	"Nash" : { "Age" : 34, "Occupation" : "Chef", "Connections" : { "John" : "cousin", "Zalika" : "landlord" }}
+}
 
-my_group =
+my_group = ss3698
