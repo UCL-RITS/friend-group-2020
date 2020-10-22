2d679116842af567148be6072a6c90fdf8ace665
Jonathan Balasingham <guest@guests-MacBook-Pro.local>
2020-10-22 11:23:34 +0100

    actually the final commit

--- group.py
+++ group.py
@@ -1,5 +1,14 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
 
-my_group =
+def main():
+    Jill = {"name": "Jill", "age": 26, "job": "biologist", "connections": {"friend": ["Zalika"], "partner": ["John"]}}
+    Zalika = {"name": "Zalika", "age": 26, "job": "artist", "connections": {"friend": ["Jill"]}}
+    John = {"name": "John", "age": 27, "job": "writer", "connections": {"partner": ["John"]}}
+    Nash = {"name": "Nash", "age": 34, "job": "chef", "connections": {"cousin": ["John"], "landlord": ["Zalika"]}}
+
+    my_group = [Jill, Zalika, John, Nash]
+    print(my_group)
+
+
+main()
