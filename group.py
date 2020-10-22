f8c59b32ff6b29e8aa3da0fb1f73209dccfeadfa
David Smith <djksmith95@gmail.com>
2020-10-22 11:56:33 +0200

    created friendship group dict structure

--- group.py
+++ group.py
@@ -1,5 +1,30 @@
-"""An example of how to represent a group of acquaintances in Python."""
+group = {
+    'Jill' : {
+         'age': 26,
+         'job': 'biologist',
+         'connection': {
+             'Zalika':'friend',
+             'John':'partner'
+         }},
+     'Zalika' : {
+         'age': 28,
+         'job': 'artist',
+         'connection': {
+             'Jill':'friend'
+         }},
+    'John' : {
+         'age': 27,
+         'job': 'writer',
+         'connection': {
+             'Jill':'partner'
+         }},     
+    'Nash' : {
+         'age': 34,
+         'job': 'chef',
+         'connection': {
+             'John':'cousin',
+             'Zalika':'landlord'
+         }}   
+}
 
-# Your code to go here...
-
-my_group =
+print(group.items())
