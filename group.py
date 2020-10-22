cc38eeb465067f06b10a27d04031f055a272d0ed
NosVladimir <zwxspecial@163.com>
2020-10-22 17:52:45 +0800

    First try

--- group.py
+++ group.py
@@ -1,5 +1,13 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+person1 = {"name" : "Person1", "age" : 20,
+           "job" : "student"}
+person2 = {"name" : "Person2", "age" : 20,
+           "job" : "student"}
 
-my_group =
+person1["classmate"] = person2
+
+my_group = [person1, person2]
+
+print(person1["name"])
