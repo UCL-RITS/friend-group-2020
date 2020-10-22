c01a393b5c34245a65bedf41daf91a62e8ef0569
David Scobie <david.scobie.20@ucl.ac.uk>
2020-10-22 11:24:10 +0100

    Making dictionary of details

--- group.py
+++ group.py
@@ -1,12 +1,12 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-Jill = {"name": "Jill", "age": 26, 
-           "Jobs": "Biologist", "Aquaintances" : {"Zalika":"Friend","John"}}
-Zalika = {"name": "Zalika", "age": 28, 
-           "Jobs": "Artist", "Aquaintances" : ""}
-John = {"name": "John", "age": 27, 
-           "Jobs": "Writer", "Aquaintances" : ""}   
-Nash = {"name": "Nash", "age": 34, 
-           "Jobs": "Chef", "Aquaintances" : ""} 
+
       
-my_group =
+my_group ={"Jill":{"name": "Jill", "age": 26, 
+           "Jobs": "Biologist", "Aquaintances" : {"Zalika":"Friend","John":"Partner"}}
+"Zalika":{"name": "Zalika", "age": 28, 
+           "Jobs": "Artist", "Aquaintances" : {"Jill":"Friend"}}
+"John":{"name": "John", "age": 27, 
+           "Jobs": "Writer", "Aquaintances" : {"Jill":"Partner"}} 
+"Nash":{"name": "Nash", "age": 34, 
+           "Jobs": "Chef", "Aquaintances" : {"John":"Cousin","Zalika":"Landlord"}} }
