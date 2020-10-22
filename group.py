2f4def6626b43eb95ba277957d29303f792db1d7
Thomas Wemyss <twemyss@users.noreply.github.com>
2020-10-22 12:31:33 +0100

    Add code to filter list and extract details of ages

--- group.py
+++ group.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import numpy as np
 
 """An example of how to represent a group of acquaintances in Python."""
 
@@ -8,7 +9,6 @@ class Relation(Enum):
      FRIEND = 2
      LANDLORD = 3
      COUSIN = 4
-
 
 # Store data as a dict of dict's, where the key is the ID of the user which is used in relations
 my_group = {
@@ -46,3 +46,34 @@ my_group = {
                     ]
                 },
         }
+
+# Firstly begin by looking at all the users and their ages, to get the average and maximum age globally
+
+# Loop through and make a list of the ages
+ages = [friend['age'] for friend in my_group.values()]
+# Print maximum and average
+print("Maximum age in group: ", np.max(ages))
+print("Average age in group: ", np.average(ages))
+
+# Now find the maximum age of people in the group that have at least one relation
+ages = [friend['age'] for friend in my_group.values() if len(friend['relations']) >= 1]
+# Print maximum and average
+print("Maximum age of people in group with at least one relation: ", np.max(ages))
+
+# Now find the maximum age of people in the group that have at least one friend
+
+# Firstly, using a simple for loop
+ages = []
+for friend in my_group.values():
+    num_friends = 0
+    for relation in friend['relations']:
+        if relation['type'] == Relation.FRIEND:
+            num_friends = num_friends + 1
+    if num_friends >= 1:
+        ages.append(friend['age'])
+print("Maximum age people in the group that have at least one friend (for loop): ", np.max(ages))
+
+# Then an alternative where it's written as one line using list comprehension expressions
+ages = [friend['age'] for friend in my_group.values() if [relation['type'] for relation in friend['relations']].count(Relation.FRIEND) >= 1]
+# Print maximum and average
+print("Maximum age people in the group that have at least one friend (list comprehension): ", np.max(ages))
