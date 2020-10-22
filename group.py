94b82b0a7c3705abdb4629a2fd4e61b4ca89af95
NosVladimir <zwxspecial@163.com>
2020-10-22 18:22:31 +0800

    Template

--- group.py
+++ group.py
@@ -1,13 +1,31 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
-person1 = {"name" : "Person1", "age" : 20,
-           "job" : "student"}
-person2 = {"name" : "Person2", "age" : 20,
-           "job" : "student"}
+jill = {"name": "Jill", "age": 26,
+        "job": "biologist"}
 
-person1["classmate"] = person2
+zalika = {"name": "Zalika", "age": 28,
+          "job": "artist"}
 
-my_group = [person1, person2]
+john = {"name": "John", "age": 27,
+        "job": "writer"}
 
-print(person1["name"])
+nash = {"name": "Nash", "age": 0,
+        "job": "chef"}
+
+jill["friend"] = zalika
+jill["partner"] = john
+jill["cousin"] = nash
+zalika["friend"] = jill
+zalika["landlord"] = nash
+john["partner"] = jill
+
+example_group = [jill, zalika, john, nash]
+
+weixi = {"name": "Weixi Zhang", "age": 20,
+        "job": "student"}
+# aaa = ...
+my_group = [weixi] #, aaa
+
+print(my_group[2]["job"])
+
