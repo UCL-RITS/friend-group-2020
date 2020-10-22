8c5914a7d3dca621e3d940a1d51faabdcfd1d128
Andrius Vaitkus <andrius.vaitkus.16@ucl.ac.uk>
2020-10-22 10:59:14 +0100

    added group information into the group.py script

--- group.py
+++ group.py
@@ -2,4 +2,26 @@
 
 # Your code to go here...
 
-my_group =
+my_group = [{
+	     	"name":"Jill", 
+	     	"age":26,
+	     	"occupancy":"biologist",
+	     	"relationships":{"friend":"Zalika", "partner":"John"}
+	}, {
+		"name":"Zalika",
+		"age":28,
+		"occupancy":"artist",
+		"relationships":{"friend":"Jill"}
+	}, {
+		"name":"John",
+		"age":27,
+		"occupancy":"writer",
+		"relationships":{"partner":"Jill"}
+	}, {
+		"name":"Nash",
+		"age":34,
+		"occupancy":"chef",
+		"relationships":{"cousin":"John", "landlord":"Zalika"}
+	}]
+
+
