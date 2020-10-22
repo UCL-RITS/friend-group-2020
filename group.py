42de5488a491a8d7a622953f5c37a23a34738b7d
poppynikou <poppyrebecca@hotmail.co.uk>
2020-10-22 14:44:22 +0100

    added comprehensions for different scenarios

--- group.py
+++ group.py
@@ -1,6 +1,7 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+import numpy as np
 
 class Person:
 
@@ -27,4 +28,19 @@ John.add_relationship("Jill", "partner")
 Nash.add_relationship("John","cousin")
 Nash.add_relationship("Zalika","landlord")
 
+
+people = [Jill, Zalika, John, Nash]
+
+#printing the maximum age of a person in the group using comprehension
+print(np.max([person.age for person in people]))
+
+#prining the average(mean) number of relations of the group using comprehension
+print(np.mean([len(person.relationships) for person in people]))
+
+#printing the maximum age of the person in the group which has atleast one relation 
+print(np.max([person.age for person in people if len(person.relationships) >= 1]))
+
+
+#printing the maximum age of the person in the group which has atleast one friend 
+print(np.max([person.age for person in people for rel in person.relationships.values() if rel== 'friend']))
 
