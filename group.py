2aae9e9d66b02fe1ebcaf332fbd286e7e89f9825
David Smith <djksmith95@gmail.com>
2020-10-22 13:53:03 +0200

    Answers for Issue 10 homework

--- group.py
+++ group.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 group = {
     'Jill' : {
          'age': 26,
@@ -27,4 +29,18 @@ group = {
          }}   
 }
 
-print(group.items())
+# 1) Get maximum age of group 
+
+print(max(person['age'] for person in group.values()))
+
+# 2) Get mean number of relations
+
+print(np.mean([len(p['connection']) for p in group.values()]))
+
+# 3) Get maxmium age of people that have at least one relation
+
+print(max(person['age'] for person in group.values() if len(person['connection']) > 1))
+
+# 4) Get maxmium age of people that have at least one friend 
+
+print(max(person['age'] for person in group.values() if 'friend' in person['connection'].values()))
