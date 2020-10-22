ef203f4d5deed4a31137f5214213f3b8433a5da4
Thomas Wemyss <twemyss@users.noreply.github.com>
2020-10-22 10:57:58 +0100

    Reformat code and use enum for relation types

--- group.py
+++ group.py
@@ -1,9 +1,52 @@
+from enum import Enum
+
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
+# Make a enum storing possible relation types and an ID for that relationship type
+class Relation(Enum):
+     PARTNER = 1
+     FRIEND = 2
+     LANDLORD = 3
+     COUSIN = 4
 
-my_group = [ {'id': 1, 'name': 'Jill', 'job': 'Biologist', 'age': 26, 'relations': [{'to': 2, 'type': 'Friend'}, {'to': 3, 'type': 'Partner'}]},
-{'id': 2, 'name': 'Zalika', 'job': 'Artist', 'age': 28, 'relations': [{'to': 1, 'type': 'Friend'}]},
-{'id': 3, 'name': 'John', 'job': 'Writer', 'age': 27, 'relations': [{'to': 1, 'type': 'Partner'}]},
-{'id': 4, 'name': 'Nash', 'job': 'Chef', 'age': 34, 'relations': [{'to': 3, 'type': 'Cousin'}, {'to': 2, 'type': 'Landlord'}]},
-]
+
+# Store data as a list of dict's
+my_group = [
+                {
+                    'id': 1,
+                    'name': 'Jill',
+                    'job': 'Biologist',
+                    'age': 26,
+                    'relations': [
+                        {'to': 2, 'type': Relation.FRIEND},
+                        {'to': 3, 'type': Relation.PARTNER}
+                    ]
+                },
+                {
+                    'id': 2,
+                    'name': 'Zalika',
+                    'job': 'Artist',
+                    'age': 28,
+                    'relations': [
+                        {'to': 1, 'type': Relation.FRIEND}
+                    ]
+                },
+                {
+                    'id': 3,
+                    'name':'John',
+                    'job': 'Writer',
+                    'age': 27,
+                    'relations': [
+                        {'to': 1, 'type': Relation.PARTNER}
+                    ]},
+                {
+                    'id': 4,
+                    'name': 'Nash',
+                    'job': 'Chef',
+                    'age': 34,
+                    'relations': [
+                        {'to': 3, 'type': Relation.COUSIN},
+                        {'to': 2, 'type': Relation.LANDLORD}
+                    ]
+                },
+            ]
