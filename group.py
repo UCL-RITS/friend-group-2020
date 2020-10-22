dbdbbb769a4a876345f99d4fb7751fee3da5c306
congjun xie <1020303191@qq.com>
2020-10-22 18:33:50 +0800

    update by Congjun

--- group.py
+++ group.py
@@ -7,3 +7,12 @@ number_of_people = 4
 temp_time = 10/22/2020
 
 Maochun xu, age 22, a student who comes from china
+
+my group = {
+    'Congjun Xie': {
+        'Age': '22',
+        'Job': 'student'
+        'partner': ['Kaiyuan Hu', 'maochun xu', 'Yuzhe Ren']
+    }
+}
+
