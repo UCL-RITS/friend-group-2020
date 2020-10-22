f96ddfee4e35903f2207462565e5808b41342f09
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2020-10-22 10:54:28 +0100

    Added my group data structure

--- group.py
+++ group.py
@@ -2,4 +2,12 @@
 
 # Your code to go here...
 
-my_group =
+my_group = { "Jill" : {"age" : 26, "job" : "biologist", "connection" : ["Zalika", "John"]}, 
+           
+            "Zaklika" : {"age" : 28, "job" : "artist", "connection" : ["Jill"]},
+           
+            "John" : {"age" : 27, "job" : "writer", "connection" : ["Jill"]},
+           
+           "Nash" : {"age" : 34, "job" : "chef", "connection" : ["Zalika", "John"]}}
+
+
