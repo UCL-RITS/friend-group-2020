2d8bcf9d5b7f31140c3d4441e478d72b6cbe2164
Kumail <kumail.kermalli.16@ucl.ac.uk>
2020-10-22 21:04:49 +0100

    Print mean number of relations for members

--- group.py
+++ group.py
@@ -36,7 +36,13 @@ group = {
 }
 
 # maximum age
-print("The maximum age of the people in the group is:", max({room['age']for name, room in group.items()}))
+print("The maximum age of the people in the group is:", max({room['age']for name, room in group.items
+
+# average (mean) number of relations among members of the group
+relations_person = {len(room['relations']) for name, room in group.items()}
+mean = sum(relations_person)/len(relations_person)
+print("The average (mean) number of relations among members of the group:", mean)
+
 
 
 
