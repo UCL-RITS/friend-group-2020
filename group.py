fdd0e2693d69851cdc908ceb6d4ef7aea27f1ed3
Jianyue <jianyue.wang.20@ucl.ac.uk>
2020-10-22 18:44:10 +0800

    add

--- group.py
+++ group.py
@@ -6,10 +6,9 @@
 a = [{"name": "Jill", "age": 26,
       "job": "Biologist", "relation": {"friend": "Zalika", "partner": "John"}},
      {"name": "Zalika", "age": 28,
-      "job": " artist", "relation": "Jill's friend"},
+      "job": " artist", "relation": {"friend": "Jill"}},
      {"name": "John", "age": 27,
-      "job": " writer", "relation": "Jill's partner"},
+      "job": " writer", "relation": {"partner": "Jill"}},
      {"name": "Nash", "age": 34,
-      "job": " chef", "relation": "John's cousin and Zalika's landlord"}
-     ]
+      "job": " chef", "relation": {"cousin": "Jill", "landlord": "Zalika"}}]
 print(a)
