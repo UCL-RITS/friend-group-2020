8fa5bbaacc9f3ef4801da0def322884f4aed3feb
cristinafni <wmhkcfi@ucl.ac.uk>
2020-10-23 22:45:06 +0100

    json file with group code

---
+++ group_new.py
@@ -0,0 +1,48 @@
+import json 
+
+group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
+
+
+
+with open('group_file.json', 'w') as f:
+    json.dump(group, f, indent=4)
+
+
+with open('group_file.json', 'r') as json_file:
+    my_group = json_file.read()
+
+
+print(my_group)
+
+
