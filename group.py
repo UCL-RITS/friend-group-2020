2aaa1288458d5c7acdcfe5813b41959630611706
HU <uacpkhu@ucl.ac.uk>
2020-10-24 13:48:58 +0800

    add code to analyse ages

--- group.py
+++ group.py
@@ -1,4 +1,4 @@
-"""An example of how to represent a group of acquaintances in Python."""
+#An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
 
@@ -34,3 +34,13 @@ my_group = {
         }
     }
 }
+
+ages = [my_group[key]['age'] for key in my_group.keys()]
+relations = [my_group[key]['relations'] for key in my_group.keys()]
+ages_with_r = [my_group[key]['age'] for key in my_group.keys() if my_group[key]['relations']]
+ages_with_f = [my_group[key]['age'] for key in my_group.keys() if 'friend' in my_group[key]['relations'].values() ]
+
+print('the maximum age of people in the group: %d' %max(ages));
+print('the average (mean) number of relations among members of the group: %.2f' %(sum(ages)/len(ages)))
+print('the maximum age of people in the group that have at least one relation: %d' %max(ages_with_r))
+print('the maximum age of people in the group that have at least one friend: %d' %max(ages_with_f))
