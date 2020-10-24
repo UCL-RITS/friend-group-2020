874798579d50db416977f4bf7969fa93d27daeb6
William <william.pegler@kinetic-solutions.co.uk>
2020-10-24 14:13:33 +0100

    comprehension in python

--- group.py
+++ group.py
@@ -32,3 +32,35 @@ my_group = {
         }
     }
 }
+
+
+def oldest_person(group):
+    return max([person['age'] for person in group.values()])
+
+
+def mean_relations_count(group):
+    relations = [len(person['relations']) for person in group.values()]
+    return sum(relations)/len(relations)
+
+
+def oldest_person_with_relations(group):
+    relations = {key: value for (key, value) in group.items()
+                 if len(value['relations'])}
+    return oldest_person(relations)
+
+
+def oldest_person_with_friends(group):
+    relations = {key: value for (key, value) in group.items()
+                 if "friend" in value['relations'].values()}
+    return oldest_person(relations)
+
+# [more advanced] the maximum age of people in the group that have at least one friend
+
+
+print(f"the oldest person is {oldest_person(my_group)} years old")
+print(
+    f"the average numper of relations among the group is {mean_relations_count(my_group)}")
+print(
+    f"the oldest person with relations is {oldest_person_with_relations(my_group)} years old")
+print(
+    f"the oldest person with friends is {oldest_person_with_friends(my_group)} years old")
