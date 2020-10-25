4844ce3abee75c4bcc8dd5a87ab49e4f94f3e30e
Vivian <vivian.barli.17@ucl.ac.uk>
2020-10-25 17:38:32 +0000

    Doing Exercise 3

--- group.py
+++ group.py
@@ -2,4 +2,58 @@
 
 # Your code to go here...
 
-my_group =
+import statistics
+
+my_group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
+
+#finding maximum age
+max_age = max(person['age'] for person in my_group.values())
+print(max_age)
+
+#finding person w age 28 (my own exercise)
+for person in my_group:
+    if my_group[person]["age"] == 28:
+        print(person)
+
+#finding mean number of relations 
+num_of_relations = statistics.mean(len(person['relations']) for name, person in my_group.items())
+print(num_of_relations)
+
+#max age with at least one relation
+max_age_one_relation = max(person['age'] for person in my_group.values() if len(person['relations']) > 0)
+print(max_age_one_relation)
+
+#max age with at least one friend
+max_age_one_friend = max(person['age'] for person in my_group.values() if "friend" in person["relations"].values())
+print(max_age_one_friend)
