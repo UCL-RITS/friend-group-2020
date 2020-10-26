0bdf81c9f2515c712927abc2a0a4b2e55722a198
Vivian <vivian.barli.17@ucl.ac.uk>
2020-10-26 10:38:54 +0000

    Making changes to read and write json file

--- group.py
+++ group.py
@@ -1,8 +1,7 @@
 """An example of how to represent a group of acquaintances in Python."""
 
-# Your code to go here...
-
 import statistics
+import json
 
 my_group = {
     "Jill": {
@@ -38,22 +37,32 @@ my_group = {
 }
 
 #finding maximum age
-max_age = max(person['age'] for person in my_group.values())
-print(max_age)
+#max_age = max(person['age'] for person in my_group.values())
+#print(max_age)
 
 #finding person w age 28 (my own exercise)
-for person in my_group:
-    if my_group[person]["age"] == 28:
-        print(person)
+#for person in my_group:
+#    if my_group[person]["age"] == 28:
+#        print(person)
 
 #finding mean number of relations 
-num_of_relations = statistics.mean(len(person['relations']) for name, person in my_group.items())
-print(num_of_relations)
+#num_of_relations = statistics.mean(len(person['relations']) for name, person in my_group.items())
+#print(num_of_relations)
 
 #max age with at least one relation
-max_age_one_relation = max(person['age'] for person in my_group.values() if len(person['relations']) > 0)
-print(max_age_one_relation)
+#max_age_one_relation = max(person['age'] for person in my_group.values() if len(person['relations']) > 0)
+#print(max_age_one_relation)
 
 #max age with at least one friend
-max_age_one_friend = max(person['age'] for person in my_group.values() if "friend" in person["relations"].values())
-print(max_age_one_friend)
+#max_age_one_friend = max(person['age'] for person in my_group.values() if "friend" in person["relations"].values())
+#print(max_age_one_friend)
+
+#saving and loading data as json file
+
+with open ('my_group.json','w') as json_file: 
+    json.dump(my_group,json_file, indent=4)
+
+with open ('my_group.json','r') as json_file:
+    my_group_as_string = json_file.read()
+
+my_group_json = json.loads(my_group_as_string)
