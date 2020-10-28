a64dd2383c2762ed28eaa36b8e318c5a664e387c
Time Spain <luwei.cai.20@ucl.ac.uk>
2020-10-28 16:41:06 +0800

    Answer to question #11 (JSON)

--- group.py
+++ group.py
@@ -1,34 +1,42 @@
 """An example of how to represent a group of acquaintances in Python."""
 
 # Your code to go here...
+
+import json
+
 my_group = {
-    'Jill' : {
-         'age': 26,
-         'job': 'biologist',
-         'connection': {
-             'Zalika':'friend',
-             'John':'partner'
-         }},
-     'Zalika' : {
-         'age': 28,
-         'job': 'artist',
-         'connection': {
-             'Jill':'friend'
-         }},
-    'John' : {
-         'age': 27,
-         'job': 'writer',
-         'connection': {
-             'Jill':'partner'
-         }},     
-    'Nash' : {
-         'age': 34,
-         'job': 'chef',
-         'connection': {
-             'John':'cousin',
-             'Zalika':'landlord'
-         }}   
-}
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "connection": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "connection": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "connection": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "connection": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+} 
+
 
 print(my_group.items())
 
@@ -45,5 +53,17 @@ age_2 = {friend: person["age"] for friend, person in my_group.items() if len(my_
 print("The maximum age of people in the group that have at least one relation is:", max(age_2.values())) 
 
 #Question 4#
-age_3 = {friend: person["age"] for friend, person in my_group.items() if "Friend" in my_group[friend]['connection'][0].values()}
+age_3 = {friend: person["age"] for friend, person in my_group.items() if "friend" in my_group[friend]['connection'].values()}
 print("The maximum age of people in the group that have at least one friend is:", max(age_3.values()))
+
+# Change the group dictionary into the json file
+with open('my_group.json', 'r') as json_file:
+    print(json.dump(my_group, json_file, indent=4))
+
+#Loading data
+with open('my_group.json', 'r') as json_file:
+    my_group_as_string = json_file.read()
+    print(my_group_as_string)
+
+    mygroup = json.loads(my_data_as_string)
+    print(mygroup)
