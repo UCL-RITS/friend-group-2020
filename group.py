9533b78b34a05ba91d7f5a12111eb703bc10aa6d
Yulin <yulin.j@outlook.com>
2020-10-28 23:16:57 +0800

    W3HW

--- group.py
+++ group.py
@@ -1,5 +1,56 @@
 """An example of how to represent a group of acquaintances in Python."""
+import json
+import yaml
 
-# Your code to go here...
+group = {
+    "Jill": {
+        "age": 26,
+        "job": "biologist",
+        "relations": {
+            "Zalika": "friend",
+            "John": "partner"
+        }
+    },
+    "Zalika": {
+        "age": 28,
+        "job": "artist",
+        "relations": {
+            "Jill": "friend"
+        }
+    },
+    "John": {
+        "age": 27,
+        "job": "writer",
+        "relations": {
+            "Jill": "partner"
+        }
+    },
+    "Nash": {
+        "age": 34,
+        "job": "chef",
+        "relations": {
+            "John": "cousin",
+            "Zalika": "landlord"
+        }
+    }
+}
 
-my_group =
+# save as a json file
+with open('group.json', 'w') as f:
+    json.dump(group, f)
+
+with open('group.json', 'r') as f:
+    group_json = f.read()
+
+group_json = json.loads(group_json)
+print(group_json)
+
+# save as a yaml file
+
+with open("group.yaml", "w") as f:
+    yaml.dump(group, f)
+
+with open("group.yaml", "r") as f:
+    group_yaml = yaml.load(f)
+
+print(yaml.safe_dump(group_yaml))
