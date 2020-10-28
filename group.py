a10149af82b619258ef162b8320bcd1e3f72b535
kyrlazari <48288612+kyrlazari@users.noreply.github.com>
2020-10-28 21:18:21 +0100

    Adding a disctionary for relationships

--- group.py
+++ group.py
@@ -4,19 +4,22 @@
 
 my_group = {"Kyriazis":{"age":25,
                         "job":"Msc Student",
-                        "friends":["Mathew","Michael"]
+                        "relations":{"Mathew","Friend"}
                         },
             "Mathew":{"age":24,
                     "job":"Phd Student",
-                    "friends":["Kyriazis","Michael"]
+                    "relations":{"Kyriazis":"Friend","Michael":"Friend"}
                     },
             "Michael":{"age":25,
                     "job":"Phd Student",
-                    "friends":["Kyriazis","Mathew"]
+                    "relations":{"Kyriazis":"Friend","Mathew":"Friend"}
                     },
             "Emre":{"age":22,
                     "job":"Mres",
-                    "friends":["Kyriazis","Michael","Mathew"]
+                    "relations":{"Kyriazis":"Friend","Mathew":"Friend","Michael":"Friend"}
                     }        
             } 
-print(my_group["Emre"]["friends"][2])                               
+print(f"All relations of Emre {my_group['Emre']['relations']}")                               
+
+print(f"Relationship between Emre and Kyriazis {my_group['Emre']['relations']['Kyriazis']}")   
+
