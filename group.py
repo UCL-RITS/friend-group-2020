6714fe684c8c84756e58b35f5c041433a8490857
何田田 <tiantianhe@hetiantians-MacBook-Pro.local>
2020-10-29 09:20:57 +0000

    our group work for friend data structure

--- group.py
+++ group.py
@@ -2,4 +2,11 @@
 
 # Your code to go here...
 
-my_group =
+mygroup = { "Jill" : { "Age": 26, "Job": ["Biologist"], "Connections": [{"Zalika" : "friend", "John": "Partner"}] },
+
+"Zalika" : { "Age": 28, "Job": ["Artist"], "Connections": [{"Jill" : "Friend", "Nash": "Tenant"}]},
+
+"John" : { "Age": 27, "Job": ["Writer"], "Connections": [{"Jill" : "Partner", "Nash": "Cousin"}]},
+
+"Nash" : { "Age": 34, "Job": ["Chef"], "Connections": [{"John" : "Cousin", "Zalika": "Landlord"}]}
+}
