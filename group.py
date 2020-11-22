e36fb287c84cf2b9b50302dc612730c3e7f4c0c3
Christina <christina.katsiva.20@ucl.ac.uk>
2020-11-22 10:20:14 +0000

    HW3 json

--- group.py
+++ group.py
@@ -42,3 +42,13 @@ print(max(person["age"] for person in group.values()))
 print(mean([len(person["relations"]) for person in group.values()]))
 print(max(person["age"] for person in group.values() if person["relations"]))
 print(max(person["age"] for person in group.values() if "friend" in person["relations"].values()))
+
+import json
+
+#write file
+with open('group_file.json', 'w') as json_file:
+	json.dumps(group, json_file, indent=4)
+
+#read file
+with open('group_file.json', 'r') as json_file:
+	group_data = json_file.read()
